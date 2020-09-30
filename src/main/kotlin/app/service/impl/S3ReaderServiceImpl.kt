1e81ac3e2d2abeeeea1b3ee648e9fba9f9030ef6
Connor Avery <connor.avery@infinityworks.com>
2020-09-30 16:34:51 +0100

    WIP WIP WIP S3 object fetch and filtering

--- src/main/kotlin/app/service/impl/S3ReaderServiceImpl.kt
+++ src/main/kotlin/app/service/impl/S3ReaderServiceImpl.kt
@@ -18,103 +18,130 @@ import uk.gov.dwp.dataworks.logging.DataworksLogger
 class S3ReaderServiceImpl(val s3Client: AmazonS3,
                           val bucket: String,
                           val basePath: String,
-                          val collectionPaths: List<String>,
+                          val sourceDatabasePaths: List<String>,
                           val filenameFormatRegexPattern: String,
                           val filenameFormatDataExtensionPattern: String,
                           val keyPairGeneratorService: KeyPairGeneratorImpl,
                           val s3Helper: S3Helper) : S3ReaderService {
 
     override fun getCollectionSummaries(): List<CollectionSummary> {
-//        s3://bucket/basepath/collectionpath/collection.tables/
-        val collectionSummaries = getCollectionSummariesFromCollectionPaths()
+//        s3://bucket/basepath/sourceDbPath/DateIncluded/collection.tables.file
+// Fn returns list of objects; each object is an instance of collection, containing collection name and size properties
 
-        val directoryOfCollectionFolders = getListOfObjectsInPath(basePath)
+        val collectionObjectsList = sourceDatabasePaths.forEach() { sourceDatabasePath ->
+            getCollectionObjectsInPath(bucket, basePath, sourceDatabasePath)
 
-        if (directoryOfCollectionFolders == null || directoryOfCollectionFolders.isEmpty()) {
-            logger.error("No collection summaries to be viewed")
-            return listOf()
         }
-
-        val collectionSummaryList = mutableListOf<CollectionSummary>()
-
-        directoryOfCollectionFolders.forEach {
-            val collectionList = getListOfObjectsInPath(it.key)
-
-            val collectionSummary = getCollectionSummary(it.key, collectionList!!)
-
-            logger.info("Got a collection summary",
-                    "key" to collectionSummary.collectionName,
-                    "size" to collectionSummary.size.toString())
-
-            collectionSummaryList.add(collectionSummary)
-        }
-
-        logger.info("Gathered collections to be provisioned into Hbase",
-                "number_of_collections" to collectionSummaryList.size.toString())
-
-        return collectionSummaryList
+        return collectionObjectsList
     }
 
-    private fun getCollectionSummariesFromCollectionPaths(): Any {
-        collectionPaths.forEach { collectionPath ->
-            val collectionPathPrefix = "$basePath/$collectionPath"
-
-            getListOfObjectsInPath(collectionPathPrefix)
-        }
-    }
-
-    private fun getListOfObjectsInPath(objectPath: String): List<S3ObjectSummaryPair> {
+    fun getCollectionObjectsInPath(fullS3BasePath: String, sourceDatabasePath: String): List {
+        // Create objects for each collection
+        //  s3://bucket/basepath/sourceDbPath/DateIncluded/?
         try {
-            val request = ListObjectsV2Request().withBucketName(bucket).withPrefix(objectPath).withMaxKeys(1000).with
-
+            val request = ListObjectsV2Request().withBucketName(bucket).withPrefix(sourceDatabasePath).withMaxKeys(1000)
             var results: ListObjectsV2Result?
             val objectSummaries: MutableList<S3ObjectSummary> = mutableListOf()
 
             do {
-                logger.info("Getting paginated results", "s3_location", "s3://$bucketName/$fullPrefix")
                 results = s3Helper.listObjectsV2Result(s3Client, request, objectSummaries)
                 request.continuationToken = results?.nextContinuationToken
+
             } while (results != null && results.isTruncated)
+            val filteredObjects = filterResultsForDataFilesOnly(objectSummaries, filenameFormatRegexPattern, filenameFormatDataExtensionPattern)
+            deduplicateAndCarryOverCollectionPropertiesAsOne(filteredObjects)
 
-            val objectSummaryKeyMap = objectSummaries.map { it.key to it }.toMap()
-            val keyPairs =
-                    keyPairGeneratorService.generateKeyPairs(objectSummaries.map { it.key },
-                            filenameFormatRegexPattern.toRegex(),
-                            filenameFormatDataExtensionPattern.toRegex())
+            val list = listOf()
+            return
 
-            val pairs = keyPairs
-                    .map {
-                        val obj = objectSummaryKeyMap[it.dataKey]
-                        S3ObjectSummaryPair(obj)
-                    }
-                    .filter { pair -> pair.data != null }
-                    .filter { pair ->
-                        val data = pair.data!!
-                        if (data.size == 0L) {
-                            logger.info("Ignoring zero-byte pair", "data_key", data.key)
-                            logger.info("Processed records in file", "records_processed", "0", "file_name", data.key)
-                        }
+        }
+    }
 
-                        data.size > 0
-                    }
+    private fun filterResultsForDataFilesOnly(s3Results: MutableList<S3ObjectSummary>, filenameFormatRegexPattern: String, filenameFormatDataExtensionPattern: String) : List {
+//        Filters object summary from S3 for unique collection names and information
 
-            logger.info("Found valid key pairs", "s3_keypairs_found", "${pairs.size}", "s3_location", "s3://$bucket/$basePath")
-            return pairs
+        val matchedConditionObjects = listOf()
+        val fileNameFormatRegex = "$filenameFormatRegexPattern$filenameFormatDataExtensionPattern"
 
-        } catch (e: AmazonServiceException) {
-            logger.error("Amazon S3 failed to process the request", "error" to e.localizedMessage)
-        } catch (e: SdkClientException) {
-            logger.error("Amazon S3 couldn't be reached or the response wasn't able to be parsed", "error" to e.localizedMessage)
+        s3Results.forEach() { collectionObject ->
+            if (fileNameFormatRegex.toRegex().containsMatchIn(collectionObject.key) )
+                matchedConditionObjects.add(collectionObject)
+        }
+        return matchedConditionObjects
+
+    }
+
+    private fun deduplicateAndCarryOverCollectionPropertiesAsOne(filteredObjects: List) {
+//    Deduplicate objects by collection name and combine size values
+
+        val matchedCollectionObjects = listOf()
+
+        filteredObjects
         }
 
-
-
-        return mutableListOf()
     }
 
-    private fun getCollectionSummary(key: String, collectionSummaries: MutableList<S3ObjectSummary>) = CollectionSummary(getCollectionFromPath(key), collectionSummaries.sumBy { it.size.toInt() })
 
-    companion object {
-        val logger = DataworksLogger.getLogger(S3ReaderServiceImpl::class.toString())
+
+
+
+
+
+
+
+
+
+        private fun getListOfObjectsInPath(objectPath: String): List<S3ObjectSummaryPair> {
+            try {
+                val request = ListObjectsV2Request().withBucketName(bucket).withPrefix(objectPath).withMaxKeys(1000)
+                var results: ListObjectsV2Result?
+                val objectSummaries: MutableList<S3ObjectSummary> = mutableListOf()
+
+                do {
+                    logger.info("Getting paginated results", "s3_location", "s3://$bucketName/$fullPrefix")
+                    results = s3Helper.listObjectsV2Result(s3Client, request, objectSummaries)
+                    request.continuationToken = results?.nextContinuationToken
+                } while (results != null && results.isTruncated)
+
+                val objectSummaryKeyMap = objectSummaries.map { it.key to it }.toMap()
+                val keyPairs =
+                        keyPairGeneratorService.generateKeyPairs(objectSummaries.map { it.key },
+                                filenameFormatRegexPattern.toRegex(),
+                                filenameFormatDataExtensionPattern.toRegex())
+
+                val pairs = keyPairs
+                        .map {
+                            val obj = objectSummaryKeyMap[it.dataKey]
+                            S3ObjectSummaryPair(obj)
+                        }
+                        .filter { pair -> pair.data != null }
+                        .filter { pair ->
+                            val data = pair.data!!
+                            if (data.size == 0L) {
+                                logger.info("Ignoring zero-byte pair", "data_key", data.key)
+                                logger.info("Processed records in file", "records_processed", "0", "file_name", data.key)
+                            }
+
+                            data.size > 0
+                        }
+
+                logger.info("Found valid key pairs", "s3_keypairs_found", "${pairs.size}", "s3_location", "s3://$bucket/$basePath")
+                return pairs
+
+            } catch (e: AmazonServiceException) {
+                logger.error("Amazon S3 failed to process the request", "error" to e.localizedMessage)
+            } catch (e: SdkClientException) {
+                logger.error("Amazon S3 couldn't be reached or the response wasn't able to be parsed", "error" to e.localizedMessage)
+            }
+
+
+
+            return mutableListOf()
+        }
+
+        private fun getCollectionSummary(key: String, collectionSummaries: MutableList<S3ObjectSummary>) = CollectionSummary(getCollectionFromPath(key), collectionSummaries.sumBy { it.size.toInt() })
+
+        companion object {
+            val logger = DataworksLogger.getLogger(S3ReaderServiceImpl::class.toString())
+        }
     }
-}
