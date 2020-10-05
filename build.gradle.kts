3369056e106cbcc5a4e693fb5649c9ad125036dd
Mike Winker <mike.winker@infinityworks.com>
2020-10-05 09:39:18 +0100

    Add distribution plugin

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,6 +5,7 @@ plugins {
     id("io.spring.dependency-management") version "1.0.10.RELEASE"
     kotlin("jvm") version "1.4.0"
     kotlin("plugin.spring") version "1.3.72"
+    distribution
 }
 
 group = "app"
