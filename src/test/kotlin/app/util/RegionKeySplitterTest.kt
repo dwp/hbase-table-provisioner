2ac6694b29522d2f3dc5948a033f1f63d913466b
Daniel Chicot <dan.chicot@gmail.com>
2020-10-08 15:25:49 +0100

    tidy up.

--- src/test/kotlin/app/util/RegionKeySplitterTest.kt
+++ src/test/kotlin/app/util/RegionKeySplitterTest.kt
@@ -7,20 +7,21 @@ class RegionKeySplitterTest {
 
     @Test
     fun splitsAreEquallySized() {
-        val keyspaceSize = 256 * 256
         for (regionCount in 1 .. 10_000) {
             calculateSplits(regionCount).let { splits ->
                 assertEquals(regionCount - 1, splits.size)
-                val expectedSize = keyspaceSize / regionCount
-                val remainder = keyspaceSize % regionCount
                 gaps(splits).let { gaps ->
                     gaps.indices.forEach { index ->
-                        assertEquals(expectedSize + if (index < remainder) 1 else 0, gaps[index])
+                        assertEquals(expectedSize(regionCount, index), gaps[index])
                     }
                 }
             }
         }
     }
+
+    private fun expectedSize(regionCount: Int, index: Int) =
+        minimumSize(regionCount) + if (index < remainder(regionCount)) 1 else 0
+
 
     private fun gaps(result: List<ByteArray>) =
             result.indices.map {
@@ -29,6 +30,12 @@ class RegionKeySplitterTest {
                 position2 - position1
             }
 
+    private fun remainder(regionCount: Int): Int = keyspaceSize % regionCount
+    private fun minimumSize(regionCount: Int) = keyspaceSize / regionCount
     private fun position(digits: ByteArray): Int = toUnsigned(digits[0]) * 256 + toUnsigned(digits[1])
     private fun toUnsigned(byte: Byte): Int = if (byte < 0) (byte + 256) else byte.toInt()
+
+    companion object {
+        private const val keyspaceSize = 256 * 25
+    }
 }
