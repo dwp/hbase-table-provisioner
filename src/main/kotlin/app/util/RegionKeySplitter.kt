ddfb624680af3c08fbc35c2a63df3895b92b3a10
Mark Matthews <mark.matthews@infinityworks.com>
2020-10-08 12:40:45 +0100

    MVP splitter unit tests and some better names (#14)

--- src/main/kotlin/app/util/RegionKeySplitter.kt
+++ src/main/kotlin/app/util/RegionKeySplitter.kt
@@ -1,15 +1,25 @@
 package app.util
 
-fun calculateSplits(numberRequired: Int) : List<ByteArray> {
-    val space = 256 * 256
-    val size = space / numberRequired
-    var remainder = space % numberRequired
+fun calculateSplits(numberOfRegions: Int): List<ByteArray> {
+    // We expect to make one less Split than the number of Regions required.
+    // Each split will be offset, for eaxmple;
+    // If the total bytes was 100 and we wanted 4 Regions, we would want 3  evenly spaced and sized splits:
+    //   20->39, 40->59, 60->79
+
+    // We use the first two bytes of the keys, so here need 256^2
+    val totalBytes = 256 * 256
+    val widthPerSplit = totalBytes / numberOfRegions
+    var remainder = totalBytes % numberOfRegions
     val positions = mutableListOf<Int>()
     var previous = 0
-    for (split in 0 until numberRequired - 1) {
-        val next = previous + size + (if (remainder-- > 0) 1 else 0)
+    for (split in 0 until numberOfRegions - 1) {
+        val next = previous + widthPerSplit + (if (remainder-- > 0) 1 else 0)
         positions.add(next)
         previous = next
     }
-    return positions.map {byteArrayOf((it / 256).toByte(), (it % 256).toByte())}
+    val splits = positions.map { bytePosition ->
+        //this is the start position and end position of each slice, which should be contiguous
+        byteArrayOf((bytePosition / 256).toByte(), (bytePosition % 256).toByte())
+    }
+    return splits
 }
--- src/test/kotlin/app/util/RegionKeySplitterTest.kt
+++ src/test/kotlin/app/util/RegionKeySplitterTest.kt
@@ -6,24 +6,25 @@ import org.junit.jupiter.api.Test
 class RegionKeySplitterTest {
 
     @Test
-    fun confirmCalculateSplitsWillMakeByteArrayFromMinimumNumberOfRegions() {
+    fun confirmCalculateSplitsWillMakeNoSplitsForOneRegion() {
 
         val result = calculateSplits(1)
 
-        val expected = listOf<List<Byte>>() //TODO this should not be empty
-
+        val expected = listOf<Byte>()
         assertThat(result).isEqualTo(expected)
     }
 
     @Test
-    fun confirmCalculateSplitsWillMakeByteArrayFromTypicalNumberOfRegions() {
+    fun confirmCalculateSplitsWillMakeOneLessNumberOfSplitsThanNumberOfRegions() {
 
-        val result = calculateSplits(100)
+        val result = calculateSplits(5)
+        assertThat(result.size).isEqualTo(4)
 
-        //TODO here we should be checking a list<list<byte>> not a count
-        val expectedCount = 99 //TODO Why is this off by 1?
+        val humanReadableResult = result.map { element ->
+            (element.joinToString("") { string -> String.format("\\x%02X", string) })
+        }
 
-        assertThat(result.size).isEqualTo(expectedCount)
+        assertThat(humanReadableResult.toString()).isEqualTo("[\\x33\\x34, \\x66\\x67, \\x99\\x9A, \\xCC\\xCD]")
     }
 
 }
