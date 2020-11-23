90625e730e73636b5b1590d8e5bdbb36471c4663
Daniel Chicot <dan.chicot@gmail.com>
2020-11-23 14:42:06 +0000

    Set adhoc specifications as system property.

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,10 +2,9 @@
 
 S3_PREFIXES="${1:-NOT_SET_IN_ENTRYPOINT}"
 CORRELATION_ID="${2:-NOT_SET_IN_ENTRYPOINT}"
-
-export HBASE_ADHOC_SPECIFICATIONS="$3"
+HBASE_ADHOC_SPECIFICATIONS="$3"
 
 echo "Running jar using entrypoint S3_PREFIXES=${S3_PREFIXES} and CORRELATION_ID=${CORRELATION_ID} and HBASE_ADHOC_SPECIFICATIONS=$HBASE_ADHOC_SPECIFICATIONS"
 
 export CORRELATION_ID=${CORRELATION_ID}
-java -Dcollections.prefix_paths="${S3_PREFIXES}" -jar ./hbase-table-provisioner.jar
+java -Dcollections.prefix_paths="${S3_PREFIXES}" -Dhbase.adhocSpecifications="$HBASE_ADHOC_SPECIFICATIONS" -jar ./hbase-table-provisioner.jar
